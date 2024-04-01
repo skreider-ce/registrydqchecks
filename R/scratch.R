4aba3ee28add23bff5da37770514245f6424f10f
Scott Kreider <153545676+skreider-ce@users.noreply.github.com>
2024-04-01 16:26:15 -0400

    Update scratch.R

--- R/scratch.R
+++ R/scratch.R
@@ -90,7 +90,7 @@
 #   .datasetsToCopy = c("exvisit", "exlab", "exdrugexp")
 #   ,.prelimDataFolderUrl = .prelimDataFolderUrl
 #   ,.prelimDataPullDate = .prelimDataPullDate
-#   ,.folderToCopyTo = "C:/Users/ScottKreider/Documents/scrap/testCopy/"
+#   ,.folderToCopyTo = "C:/Users/ScottKreider/Documents/scrap/abcdefg/"
 #   ,.isR = FALSE
 #   ,.remove = FALSE
 # )
