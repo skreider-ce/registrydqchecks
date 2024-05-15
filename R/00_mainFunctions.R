0e346561a2a35c9784fc9925572bbe2eae97113d
skreider-ce <skreider@corevitas.com>
2024-05-15 14:07:11 -0400

    Reference main rdcrd version on GH
    - Reference "main" rdcrd
    - Update description accordingly

--- R/00_mainFunctions.R
+++ R/00_mainFunctions.R
@@ -159,8 +159,8 @@ copyDatasetsToLocation <- function(.datasetsToCopy
 #'
 #' @export
 updatePackages <- function(snapshot = TRUE){
-  remotes::install_github("skreider-ce/registrydqchecksreportdown@*release", dependencies = TRUE) # Most up to date static package release
-  remotes::install_github("skreider-ce/registrydqchecks@*release", dependencies = TRUE) # Most up to date static package release
+  remotes::install_github("skreider-ce/registrydqchecksreportdown", dependencies = TRUE) # Most up to date static package release
+  remotes::install_github("skreider-ce/registrydqchecks", dependencies = TRUE) # Most up to date static package release
   
   if(snapshot){
     renv::snapshot()
