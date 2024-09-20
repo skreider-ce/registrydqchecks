e3235368dc5c23496673a8b7a4608712c705427f
skreider-ce <skreider@corevitas.com>
2024-09-20 10:40:34 -0400

    Update calculatePercentChange
    - Remove extraneous na.rm = TRUE

--- R/90_calculatePercentChange.R
+++ R/90_calculatePercentChange.R
@@ -28,7 +28,7 @@ calculatePercentChange <- function(data) {
     group_by(variable) |>
     mutate(
       max_percent_change = ifelse(row_type == "label", 
-                                  percentChange[which.max(abs(percentChange), na.rm = TRUE)],
+                                  percentChange[which.max(abs(percentChange))],
                                   NA_real_)
     ) |>
     ungroup() |>
