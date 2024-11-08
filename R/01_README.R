b5bba6878c8723c5774ed5a31a691a16d9ded9eb
rolson-cor <rolson@corevitas.com>
2024-11-08 09:38:12 -0700

    Addition of README
    Added README function file at 01 spot.
    Called function prior to runRegistryChecks in 00

---
+++ R/01_README.R
@@ -0,0 +1,15 @@
+#or now, I would just say to have it print something to the console. So when you run registrydqchecks::readme() it will print something like "This is some common information to keep in mind when using this package"
+
+#Define the function
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+README <- function() {
+  return("This is the ReadMe")
+}
+
+# Example of calling the function
+README()
