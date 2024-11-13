c263eeed5bd5194b7210d98889ddcae85f7c533e
rolson-cor <rolson@corevitas.com>
2024-11-13 11:21:55 -0700

    Finalization of README()
    Created README
    Update documentation
    completed check

--- R/01_README.R
+++ R/01_README.R
@@ -1,12 +1,10 @@
-#or now, I would just say to have it print something to the console. So when you run registrydqchecks::readme() it will print something like "This is some common information to keep in mind when using this package"
-
 #Define the function
-#' Title
+#' README
 #'
-#' @return
-#' @export
+#' @return Prints out text for README
+#' @export 
 #'
-#' @examples
+#' 
 README <- function() {
   return("This is the ReadMe")
 }
