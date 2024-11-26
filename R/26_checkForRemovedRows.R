5d03b33bb090433411273075ec6e5fd243489b8e
skreider-ce <skreider@corevitas.com>
2024-11-26 13:54:47 -0500

    Force cc6 listing to data frame

--- R/26_checkForRemovedRows.R
+++ R/26_checkForRemovedRows.R
@@ -15,7 +15,7 @@ checkForRemovedRows <- function(.dsToCheck,.compDsToCheck,.uniqueKey){
   .uniqueKeys <- .uniqueKey
 
   # Create dataframe of rows in .compDsToCheck and not in .dsToCheck
-  .inOldAndNotInNew <- dplyr::anti_join(.compDsToCheck,.dsToCheck,.uniqueKeys)
+  .inOldAndNotInNew <- as.data.frame(dplyr::anti_join(.compDsToCheck,.dsToCheck,.uniqueKeys))
   
   .pctRemoved <- nrow(.inOldAndNotInNew) / nrow(.compDsToCheck)
 
