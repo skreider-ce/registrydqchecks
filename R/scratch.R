0c9406760519a28e027c5f34603a60913bccbf29
skreider-ce <skreider@corevitas.com>
2025-01-29 16:34:25 -0500

    Comment out scratch

--- R/scratch.R
+++ R/scratch.R
@@ -1,37 +1,37 @@
-# Choose from aa, ad, gpp, ibd, ms, nmo, psa, pso, ra, raj
-.registryAbbreviation <- "ra"
-
-.dataPullYear <- "2025"
-.dataPullFolderDate <- "2025-01-01"
-.dataPullDate <- "2025-01-01"
-
-.lastMonthDataPullYear <- "2024"
-.lastMonthDataPullFolderDate <- "2024-12-01"
-.lastMonthDataPullDate <- "2024-12-01"
-
-source("config.R")
-
-# validateCodebook(codebookUrl = .testCodebookUrl[[.registryAbbreviation]]
-#                   ,datasetNames = .testDataSetsToCheck[[.registryAbbreviation]])
-
-# checks <- readRDS("C:/Users/ScottKreider/Documents/scrap/exampleOutput/ad_2024-10-01_2024-10-16_1602_checks.rds")
-# manualNcChecks <- checks$nonCriticalChecks
-
-outputUrl <- runRegistryChecks(.registry = .registryAbbreviation
-                  ,.prelimDataFolderUrl = .testDataFolderUrl[[.registryAbbreviation]]
-                  ,.prelimDataPullDate = .dataPullDate
-                  ,.lastMonthDataFolderUrl = .testLastMonthDataFolderUrl[[.registryAbbreviation]]
-                  ,.lastMonthDataPullDate = .lastMonthDataPullDate
-                  ,.codebookUrl = .testCodebookUrl[[.registryAbbreviation]]
-                  ,.siteInfoUrl = .configSiteInfoUrl
-                  ,.cdmRomReportUrl = .exampleRomOutputFolder
-                  ,.datasetsToCheck = .testDataSetsToCheck[[.registryAbbreviation]]
-                  ,.nonCriticalChecks = NULL
-                  ,.outputUrl = glue::glue("{.reportOutputUrl}{.registryAbbreviation}/{.dataPullYear}/{.dataPullDate}/")
-                  ,.isR = .testIsR[[.registryAbbreviation]])
-
-# copyRomListingToFolder(.reportOutputUrl = outputUrl
-#                        ,.romReportUrl = .exampleRomOutputFolder
-#                        ,.registry = .registryAbbreviation
-#                        ,.dataPullDate = .dataPullDate
-#                        ,.overwrite = FALSE)
+# # Choose from aa, ad, gpp, ibd, ms, nmo, psa, pso, ra, raj
+# .registryAbbreviation <- "ra"
+# 
+# .dataPullYear <- "2025"
+# .dataPullFolderDate <- "2025-01-01"
+# .dataPullDate <- "2025-01-01"
+# 
+# .lastMonthDataPullYear <- "2024"
+# .lastMonthDataPullFolderDate <- "2024-12-01"
+# .lastMonthDataPullDate <- "2024-12-01"
+# 
+# source("config.R")
+# 
+# # validateCodebook(codebookUrl = .testCodebookUrl[[.registryAbbreviation]]
+# #                   ,datasetNames = .testDataSetsToCheck[[.registryAbbreviation]])
+# 
+# # checks <- readRDS("C:/Users/ScottKreider/Documents/scrap/exampleOutput/ad_2024-10-01_2024-10-16_1602_checks.rds")
+# # manualNcChecks <- checks$nonCriticalChecks
+# 
+# outputUrl <- runRegistryChecks(.registry = .registryAbbreviation
+#                   ,.prelimDataFolderUrl = .testDataFolderUrl[[.registryAbbreviation]]
+#                   ,.prelimDataPullDate = .dataPullDate
+#                   ,.lastMonthDataFolderUrl = .testLastMonthDataFolderUrl[[.registryAbbreviation]]
+#                   ,.lastMonthDataPullDate = .lastMonthDataPullDate
+#                   ,.codebookUrl = .testCodebookUrl[[.registryAbbreviation]]
+#                   ,.siteInfoUrl = .configSiteInfoUrl
+#                   ,.cdmRomReportUrl = .exampleRomOutputFolder
+#                   ,.datasetsToCheck = .testDataSetsToCheck[[.registryAbbreviation]]
+#                   ,.nonCriticalChecks = NULL
+#                   ,.outputUrl = glue::glue("{.reportOutputUrl}{.registryAbbreviation}/{.dataPullYear}/{.dataPullDate}/")
+#                   ,.isR = .testIsR[[.registryAbbreviation]])
+# 
+# # copyRomListingToFolder(.reportOutputUrl = outputUrl
+# #                        ,.romReportUrl = .exampleRomOutputFolder
+# #                        ,.registry = .registryAbbreviation
+# #                        ,.dataPullDate = .dataPullDate
+# #                        ,.overwrite = FALSE)
